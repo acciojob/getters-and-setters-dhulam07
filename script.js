48e129e2c2a9a5b781a1a25f2ed17668716f2f08
dhulam07 <chandudhulam794@gmail.com>
2025-11-23 13:03:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,23 +5,30 @@ class Person {
 		this.age=age;
 	}
 	get Name(){
-		return this.name;
+		console.log(person.name);
 	}
 	set Age(age){
 		this.age=age;
+		console.log(person.age); 
 	}
+	const person = new Person("John", 25);
+
 }
 
 class Student extends Person {
 	 study(){
 		console.log(`${this.name} is studying`)
 	}
+	const student = new Student("Alice", 22);
+	student.study(); 
 }
 
 class Teacher extends Person {
 	 teach(){
 		console.log(`${this.name} is teaching`)
 	}
+	const teacher = new Teacher("Bob", 40);
+	teacher.teach(); 
 }
 
 // Do not change the code below this line
